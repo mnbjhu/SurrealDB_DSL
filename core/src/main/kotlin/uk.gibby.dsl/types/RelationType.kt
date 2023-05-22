5599ba430021a7973a638d6254ba2b25896edb76
james <james-gibson@ntlworld.com>
2023-05-22 21:55:32 +0100

    Fixing linked encoder

--- core/src/main/kotlin/uk.gibby.dsl/types/RelationType.kt
+++ core/src/main/kotlin/uk.gibby.dsl/types/RelationType.kt
@@ -14,20 +14,44 @@ fun <a, A: RecordType<a>, b, B: RelationType<a, A, b, c, C>, c, C: RecordType<c>
         else ListType(linked(to), "${getReference()}->${to.name}")
 }
 
+fun <a, A: RecordType<a>, b, B: RelationType<a, A, b, c, C>, c, C: RecordType<c>>A.`o-→`(to: Table<b, B>, where: B.() -> BooleanType): ListType<Linked<b>, RecordLink<b, B>> {
+    val filter = to.recordType.where().getReference()
+    return if(getReference() == "_") ListType(linked(to), "->(${to.name} WHERE $filter)")
+    else ListType(linked(to), "${getReference()}->(${to.name} WHERE $filter)")
+}
+
 @JvmName("rightArrow")
 fun <a, A: RecordType<a>, b, B: RelationType<a, A, b, c, C>, c, C: RecordType<c>>ListType<Linked<a>, RecordLink<a, A>>.`o-→`(to: Table<b, B>): ListType<Linked<b>, RecordLink<b, B>>{
     return if(getReference() == "_") ListType(with(inner.inner){ linked(to) } , "->" + to.name)
     else ListType(with(inner.inner){ linked(to) }, "${getReference()}->${to.name}")
 }
 
+@JvmName("rightArrow")
+fun <a, A: RecordType<a>, b, B: RelationType<a, A, b, c, C>, c, C: RecordType<c>>ListType<Linked<a>, RecordLink<a, A>>.`o-→`(to: Table<b, B>, where: B.() -> BooleanType): ListType<Linked<b>, RecordLink<b, B>>{
+    val filter = to.recordType.where().getReference()
+    return if(getReference() == "_") ListType(with(inner.inner){ linked(to) } , "->(${to.name} WHERE $filter)")
+    else ListType(with(inner.inner){ linked(to) }, "${getReference()}->(${to.name} WHERE $filter)")
+}
 fun <a, A: RecordType<a>, b, B: RelationType<a, A, b, c, C>, c, C: RecordType<c>>ListType<Linked<b>, RecordLink<b, B>>.`o-→`(to: Table<c, C>): ListType<Linked<c>, RecordLink<c, C>> {
     return if(getReference() == "_") ListType(inner.inner.out, "->${to.name}")
     else ListType(inner.inner.out, "${getReference()}->${to.name}")
 }
 
+fun <a, A: RecordType<a>, b, B: RelationType<a, A, b, c, C>, c, C: RecordType<c>>ListType<Linked<b>, RecordLink<b, B>>.`o-→`(to: Table<c, C>, where: C.() -> BooleanType): ListType<Linked<c>, RecordLink<c, C>> {
+    val filter = to.recordType.where().getReference()
+    return if(getReference() == "_") ListType(inner.inner.out, "->(${to.name} WHERE $filter)")
+    else ListType(inner.inner.out, "${getReference()}->(${to.name} WHERE $filter)")
+}
+
 fun <a, A: RecordType<a>, b, B: RelationType<c, C, b, a, A>, c, C: RecordType<c>>A.`←-o`(to: Table<b, B>): ListType<Linked<b>, RecordLink<b, B>>{
     return if(getReference() == "_") ListType(linked(to), "<-" + to.name)
     else ListType(linked(to), "${getReference()}<-${to.name}")
+}
+
+fun <a, A: RecordType<a>, b, B: RelationType<c, C, b, a, A>, c, C: RecordType<c>>A.`←-o`(to: Table<b, B>, where: B.() -> BooleanType): ListType<Linked<b>, RecordLink<b, B>>{
+    val filter = to.recordType.where().getReference()
+    return if(getReference() == "_") ListType(linked(to), "<-(${to.name} WHERE $filter)")
+    else ListType(linked(to), "${getReference()}<-(${to.name} WHERE $filter)")
 }
 
 @JvmName("leftArrow")
@@ -36,10 +60,25 @@ fun <a, A: RecordType<a>, b, B: RelationType<c, C, b, a, A>, c, C: RecordType<c>
     else ListType(inner.inner.`in`, "${getReference()}<-${to.name}")
 }
 
+@JvmName("leftArrow")
+fun <a, A: RecordType<a>, b, B: RelationType<c, C, b, a, A>, c, C: RecordType<c>>ListType<Linked<b>, RecordLink<b, B>>.`←-o`(to: Table<c, C>, where: C.() -> BooleanType): ListType<Linked<c>, RecordLink<c, C>> {
+    val filter = to.recordType.where().getReference()
+    return if(getReference() == "_") ListType(inner.inner.`in`, "<-(${to.name} WHERE $filter)")
+    else ListType(inner.inner.`in`, "${getReference()}<-(${to.name} WHERE $filter)")
+}
+
 fun <a, A: RecordType<a>, b, B: RelationType<c, C, b, a, A>, c, C: RecordType<c>>ListType<Linked<a>, RecordLink<a, A>>.`←-o`(to: Table<b, B>): ListType<Linked<b>, RecordLink<b, B>>{
     return if(getReference() == "_") ListType(with(inner.inner){ linked(to) } , "<-" + to.name)
     else ListType(with(inner.inner){ linked(to) }, "${getReference()}<-${to.name}")
 }
+
+
+fun <a, A: RecordType<a>, b, B: RelationType<c, C, b, a, A>, c, C: RecordType<c>>ListType<Linked<a>, RecordLink<a, A>>.`←-o`(to: Table<b, B>, where: B.() -> BooleanType): ListType<Linked<b>, RecordLink<b, B>>{
+    val filter = to.recordType.where().getReference()
+    return if(getReference() == "_") ListType(with(inner.inner){ linked(to) } , "<-(${to.name} WHERE $filter)")
+    else ListType(with(inner.inner){ linked(to) }, "${getReference()}<-(${to.name} WHERE $filter)")
+}
+
 val <T, U: RecordType<T>>RecordLink<T, U>.STAR: U
     get() = inner.createReference(getReference() + ".*") as U
 val <T, U: RecordType<T>>ListType<Linked<T>, RecordLink<T, U>>.STAR: ListType<T, U>
